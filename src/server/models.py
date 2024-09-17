c6190d2c30c1dfa55883f366368b673ba6abee84
Aditya Vikram Singh <adivik672@gmail.com>
2024-09-17 16:07:55 +0530

    Adds the model for server

---
+++ src/server/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+import uuid
+from django.conf import settings
+
+
+class Server(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+    )
+    name = models.CharField(max_length=200, unique=True)
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASACADE,
+        related_name="created_server",
+    )
+    
+    def __str__(self):
+        return self.name
+
